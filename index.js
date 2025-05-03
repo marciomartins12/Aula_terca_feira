a7fa2348e81c5a8e8c1ec6016ba836178d67fb12
mmarciomartins12 <mmarciom1martins635@gmail.com>
2025-05-03 20:47:08 -0300

    my first Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const app = express();
+const session = require("express-session");
+const routerAdmin = require("./routes/adminRoutes");
+
+app.engine("handlebars", engine({
+    defaultLayout: "main",
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    },
+    helpers: {
+        eq: (a, b) => a === b,
+    },
+}));
+app.set("view engine", "handlebars");
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(session({
+    secret: 'pouso9090',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(8080, () => {
+    console.log(`Servidor rodando na porta http://localhost:8080`)
+});
